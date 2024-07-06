7c92d92a6205b04713ca300153a4ce46bcf46223
Muntasir Chowdhury <muntasirornob@gmail.com>
2024-07-05 21:28:36 -0600

    basic setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Header } from './components/Header';
+import { AllRoutes } from './components/routes/AllRoutes';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AllRoutes/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/routes/AllRoutes.js
@@ -0,0 +1,19 @@
+import { Routes,Route } from "react-router-dom";
+import { MovieList,MovieDetail,Search,PageNotFound } from "../pages";
+
+
+
+export const AllRoutes = () => {
+  return (
+    <>
+    <Routes>
+        <Route path="" element= { <MovieList/> } />
+        <Route path="movie/:id" element= { <MovieDetail/> } />
+        <Route path="movies/trending" element= { <MovieList/> } />
+        <Route path="movie/popular" element= { <MovieList/> } />
+        <Route path="movie/upcoming" element= { <MovieList/> } />
+        <Route path="search" element= { <Search/> } />
+    </Routes>
+    </>
+  )
+}
