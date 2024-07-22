3e7c9ae89242f28fc2755d85f8b4a2f3dcd161e1
Muntasir Chowdhury <muntasirornob@gmail.com>
2024-07-21 21:31:39 -0600

    image added in movie list

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,16 @@
 
 import { Link } from "react-router-dom";
-export const Card = () => {
-  return (
+export const Card = ({movie}) => {
+    const {id,original_title,poster_path} = movie;
+    const img = `https://image.tmdb.org/t/p/w500/${poster_path}`
+  return ( 
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3">
                 <Link href="#">
-                    <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                    <img className="rounded-t-lg" src={img} alt="" />
                 </Link>
                 <div className="p-5">
                     <Link href="#">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{original_title}</h5>
                     </Link>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
                     <Link href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
